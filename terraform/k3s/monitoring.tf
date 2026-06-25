1973f2b447a585306a0f2c5bd3a658a16545684f
Yu Jin <qldls0307@naver.com>
2026-06-25 14:51:21 +0900

    feat: 카프카 부하테스트 대시보드 추가 (#14)
    * feat: 카프카 부하테스트 대시보드 추가
    
    * fix: 카프카 대시보드 변수화

--- terraform/k3s/monitoring.tf
+++ terraform/k3s/monitoring.tf
@@ -9,15 +9,17 @@ resource "helm_release" "monitoring" {
 
   values = [
     templatefile("${path.module}/helm-values/prometheus.yaml", {
-      prometheus_external_url   = var.prometheus_external_url
-      gateway_metrics_scheme    = var.gateway_metrics_scheme
-      gateway_metrics_targets   = var.gateway_metrics_targets
-      control_metrics_scheme    = var.control_metrics_scheme
-      control_metrics_targets   = var.control_metrics_targets
-      dispatch_metrics_scheme   = var.dispatch_metrics_scheme
-      dispatch_metrics_targets  = var.dispatch_metrics_targets
-      ec2_node_exporter_targets = var.ec2_node_exporter_targets
-    })
+      prometheus_external_url    = var.prometheus_external_url
+      gateway_metrics_scheme     = var.gateway_metrics_scheme
+      gateway_metrics_targets    = var.gateway_metrics_targets
+      control_metrics_scheme     = var.control_metrics_scheme
+      control_metrics_targets    = var.control_metrics_targets
+      dispatch_metrics_scheme    = var.dispatch_metrics_scheme
+      dispatch_metrics_targets   = var.dispatch_metrics_targets
+      ec2_node_exporter_targets  = var.ec2_node_exporter_targets
+      kafka_jmx_exporter_targets = var.kafka_jmx_exporter_targets
+    }),
+    file("${path.module}/helm-values/kafka-load-test-dashboard.yaml")
   ]
 
   set_sensitive {
@@ -55,6 +57,31 @@ resource "helm_release" "blackbox_exporter" {
   version    = var.blackbox_exporter_chart_version
 
   values = [file("${path.module}/helm-values/blackbox-exporter.yaml")]
+
+  atomic          = true
+  cleanup_on_fail = true
+  timeout         = 600
+
+  depends_on = [helm_release.monitoring]
+}
+
+resource "helm_release" "kafka_exporter" {
+  name             = "kafka-exporter"
+  namespace        = "monitoring"
+  create_namespace = false
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-kafka-exporter"
+  version    = var.kafka_exporter_chart_version
+
+  values = [
+    templatefile("${path.module}/helm-values/kafka-exporter.yaml", {
+      kafka_servers   = var.kafka_exporter_kafka_servers
+      kafka_server_ip = var.kafka_server_ip
+      topic_filter    = var.kafka_exporter_topic_filter
+      group_filter    = var.kafka_exporter_group_filter
+    })
+  ]
 
   atomic          = true
   cleanup_on_fail = true
